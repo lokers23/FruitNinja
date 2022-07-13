0869aef2983ecc3c00ce8ce83d76061f311f4b33
Vlad <pevloks@gmail.com>
2022-07-13 21:17:02 +0300

    Added the ability to edit and increase complexity over time.

--- Assets/Scripts/Spawner/Spawn.cs
+++ Assets/Scripts/Spawner/Spawn.cs
@@ -9,8 +9,18 @@ namespace Spawner
     public class Spawn : MonoBehaviour
     { 
         [SerializeField]private GameObject[] fruits;
+        
         [SerializeField] private int fruitInRow;
-        [SerializeField]private float angleRotate;
+        [SerializeField] private float periodInRow;
+        
+        [SerializeField] private int periodUpDifficult;
+        [SerializeField] private int increaseNumberFruit;
+        [SerializeField] private float limitNumberFruit;
+        [SerializeField] private float reduceSpawnDelay;
+        
+        [SerializeField]private float maxAngleRotate;
+        [SerializeField]private float minAngleRotate;
+        
         [SerializeField]private float minForce;
         [SerializeField]private float maxForce;
          
@@ -19,15 +29,22 @@ namespace Spawner
         
         [SerializeField]private float minSpawnDelay;
         [SerializeField]private float maxSpawnDelay;
-        private float random;
 
-        private void Awake()
+        private IEnumerator coroutineDifficult;
+
+        private void Update()
         {
+            if (fruitInRow >= limitNumberFruit)
+            {
+                StopCoroutine(coroutineDifficult);
+            }
         }
 
         private void OnEnable()
         {
             StartCoroutine(Spawning());
+            coroutineDifficult = IncreasingDifficulty();
+            StartCoroutine(coroutineDifficult);
         }
 
         private void OnDisable()
@@ -46,16 +63,31 @@ namespace Spawner
                     Vector2 positionFruit = new Vector2(position.x, position.y);
                     Quaternion rotation = Quaternion.Euler(0f, 0f, 0);
                     GameObject prefab = fruits[Random.Range(0, fruits.Length)];
+                    
                     float force = Random.Range(minForce, maxForce);
                     float angle = Random.Range(minAngle, maxAngle) * Mathf.Deg2Rad;
-                
+                    float rotate = Random.Range(minAngleRotate, maxAngleRotate);
+                    
                     var fruit = Instantiate(prefab, positionFruit, rotation).GetComponent<Gravity>();
                     fruit.direction = VectorFromAngle(angle) * force;
-                    fruit.rotateZ = angleRotate * Mathf.Deg2Rad;
-                    yield return new WaitForSeconds(0.5f);
+                    fruit.rotateZ = rotate * Mathf.Deg2Rad;
+                    
+                    yield return new WaitForSeconds(periodInRow);
                 }
                 
                 yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            }
+        }
+        
+        private IEnumerator IncreasingDifficulty() {
+            yield return new WaitForSeconds(periodUpDifficult);
+            for(;;)
+            {
+                fruitInRow += increaseNumberFruit;
+                minSpawnDelay -= reduceSpawnDelay;
+                maxSpawnDelay -= reduceSpawnDelay;
+                
+                yield return new WaitForSeconds(periodUpDifficult);
             }
         }
         
