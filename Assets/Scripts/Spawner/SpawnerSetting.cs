988d99663009df8161a5a2f5f2ece0fb9866f445
Vlad <pevloks@gmail.com>
2022-07-14 21:47:27 +0300

    Created SpawnerSetting

---
+++ Assets/Scripts/Spawner/SpawnerSetting.cs
@@ -0,0 +1,26 @@
+namespace Spawner
+{
+    [System.Serializable]
+    public class SpawnerSetting
+    {
+        public int fruitInRow;
+        public float periodInRow;
+        
+        public int periodUpDifficult;
+        public int increaseNumberFruit;
+        public float limitNumberFruit;
+        public float reduceSpawnDelay;
+        
+        public float maxAngleRotate;
+        public float minAngleRotate;
+        
+        public float minForce;
+        public float maxForce;
+        
+        public float minAngle;
+        public float maxAngle;
+        
+        public float minSpawnDelay;
+        public float maxSpawnDelay;
+    }
+}
