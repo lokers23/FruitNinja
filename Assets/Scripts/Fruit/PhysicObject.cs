29cbc29cab200d2cf81d4ecba09b0011248d8038
Vlad <pevloks@gmail.com>
2022-07-14 23:38:26 +0300

    Add field spriteFruit instead of calling "GetChild" method

--- Assets/Scripts/Fruit/PhysicObject.cs
+++ Assets/Scripts/Fruit/PhysicObject.cs
@@ -1,21 +1,22 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PhysicObject : MonoBehaviour
+namespace Fruit
 {
-    [SerializeField] private Vector2 gravityVector = new Vector2(0, -9.82f);
-    [SerializeField] private Vector3 speedScale = new Vector3(-0.12f, -0.12f, 0) ;
-    
-    [HideInInspector] public Vector2 direction;
-    [HideInInspector] public float rotate;
-    
-    private void Update()
+    public class PhysicObject : MonoBehaviour
     {
-        transform.localScale += speedScale * Time.deltaTime;
-        transform.GetChild(0).Rotate(new Vector3(0, 0, rotate));
-        transform.Translate(direction * Time.deltaTime);
-        direction += gravityVector * Time.deltaTime;
+        [SerializeField] private Vector2 gravityVector = new Vector2(0, -9.82f);
+        [SerializeField] private Vector3 speedScale = new Vector3(-0.12f, -0.12f, 0) ;
+        [SerializeField] private Transform spriteFruit;
+        
+        [HideInInspector] public Vector2 direction;
+        [HideInInspector] public float rotate;
+    
+        private void Update()
+        {
+            transform.localScale += speedScale * Time.deltaTime;
+            spriteFruit.Rotate(new Vector3(0, 0, rotate));
+            transform.Translate(direction * Time.deltaTime);
+            direction += gravityVector * Time.deltaTime;
+        }
     }
 }
