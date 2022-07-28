a2699f347441147f196a473be79688564ff741b5
Vlad <pevloks@gmail.com>
2022-07-28 11:06:15 +0300

    Added new fields for interacting with the bomb and the heart

--- Assets/Scripts/Spawner/Spawn.cs
+++ Assets/Scripts/Spawner/Spawn.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using Fruit;
+using Fruit.Bonus;
 using UnityEngine;
 using static System.Random;
 using Random = UnityEngine.Random;
@@ -8,11 +9,21 @@ using Random = UnityEngine.Random;
 namespace Spawner
 {
     public class Spawn : MonoBehaviour
-    { 
+    {
+        [SerializeField] private Transform poolObjects;
         [SerializeField] private GameObject[] fruits;
+        
+        [SerializeField] private GameObject bomb;
+        [SerializeField] private float chanceSpawnBomb = 0.1f;
+
+        [SerializeField] private GameObject heart;
+        [SerializeField] private float chanceSpawnHeart = 0.1f;
+        
+        [SerializeField] private float chanceSpawn;
         [SerializeField] private SpawnerSetting spawnerSetting;
         [SerializeField] private DifficultSetting difficultSetting;
 
+        [SerializeField] private HealthController healthController;
         [SerializeField] private float startingDelay = 2f;
         private IEnumerator _coroutineDifficult;
 
@@ -41,26 +52,44 @@ namespace Spawner
         private IEnumerator Spawning()
         {
             yield return new WaitForSeconds(startingDelay);
+            
             while (enabled)
             {
-                for (int i = 0; i < spawnerSetting.fruitInRow; i++)
+                int halfCount = spawnerSetting.fruitInRow / 2;
+                if (Random.Range(0.0f, 1.0f) <= chanceSpawn)
                 {
-                    var position = transform.position;
-                    Vector2 positionFruit = new Vector2(position.x, position.y);
-                    Quaternion rotation = Quaternion.Euler(0f, 0f, 0f);
-                    GameObject prefab = fruits[Random.Range(0, fruits.Length)];
+                    for (int i = 0; i < spawnerSetting.fruitInRow; i++)
+                    {
+                        var position = transform.position;
+                        Vector2 positionFruit = new Vector2(position.x, position.y);
+                        Quaternion rotation = Quaternion.Euler(0f, 0f, 0f);
+                        
+                        float force = Random.Range(spawnerSetting.minForce, spawnerSetting.maxForce);
+                        float angle = Random.Range(spawnerSetting.minAngle, spawnerSetting.maxAngle) * Mathf.Deg2Rad;
+                        float rotate = Random.Range(spawnerSetting.minAngleRotate, spawnerSetting.maxAngleRotate) * Mathf.Deg2Rad;
+                        GameObject prefab;
+                        if (halfCount > 0 && Random.Range(0.0f, 1.0f) <= chanceSpawnBomb)
+                        {
+                            prefab = bomb;
+                            halfCount--;
+                        }
+                        else if (healthController.IndexHead > 0 && halfCount > 0 && Random.Range(0.0f, 1.0f) <= chanceSpawnHeart)
+                        {
+                            prefab = heart;
+                            halfCount--;
+                        }
+                        else{
+                            prefab = fruits[Random.Range(0, fruits.Length)];
+                        }
+                        
+                        var gameObject = Instantiate(prefab, positionFruit, rotation, poolObjects).GetComponent<PhysicObject>();
+                        gameObject.direction = VectorFromAngle(angle) * force;
+                        gameObject.rotate = rotate;
                     
-                    float force = Random.Range(spawnerSetting.minForce, spawnerSetting.maxForce);
-                    float angle = Random.Range(spawnerSetting.minAngle, spawnerSetting.maxAngle) * Mathf.Deg2Rad;
-                    float rotate = Random.Range(spawnerSetting.minAngleRotate, spawnerSetting.maxAngleRotate) * Mathf.Deg2Rad;
-                    
-                    var fruit = Instantiate(prefab, positionFruit, rotation).GetComponent<PhysicObject>();
-                    fruit.direction = VectorFromAngle(angle) * force;
-                    fruit.rotate = rotate;
-                    
-                    yield return new WaitForSeconds(spawnerSetting.periodInRow);
+                        yield return new WaitForSeconds(spawnerSetting.periodInRow);
+                    }
                 }
-                
+
                 yield return new WaitForSeconds(Random.Range(spawnerSetting.minSpawnDelay, spawnerSetting.maxSpawnDelay));
             }
         }
